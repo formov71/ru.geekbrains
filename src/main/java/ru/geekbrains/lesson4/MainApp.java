3c2797f34b0c83541d0ad2f2d45b9f365c1f5546
formov71 <formov71@yandex.ru>
2021-03-10 23:34:25 +0300

    Work game no intelect

--- src/main/java/ru/geekbrains/lesson4/MainApp.java
+++ src/main/java/ru/geekbrains/lesson4/MainApp.java
@@ -7,14 +7,20 @@ import java.util.Scanner;
 public class MainApp {
 
     private static Random random = new Random();
-    private static final int SIZE_CUBE = 3;
-    private static final int COUNT_WIN_LINE = 3;
+    private static final int SIZE_CUBE = 5;
+    private static final int COUNT_WIN_LINE = 4;
     private static final String EMPTY_CELL = "o";
-    private static final String COMP_CELL = "\u001B[32m" + "O" + "\u001B[0m";
+    private static final String COMP_CELL = "\u001B[32m" + "0" + "\u001B[0m";
     private static final String GAMER_CELL = "\u001B[31m" + "X" + "\u001B[0m";
     private static String[][] WAR_MAP;
-    private static enum varCheck { HORIZONT, VERTICAL, LEFT_DIAGONAL, RIGHT_DIAGONAL };
-    private static enum whoRun { COMP, GAMER };
+
+    private static enum varCheck {HORIZONT, VERTICAL, LEFT_DIAGONAL, RIGHT_DIAGONAL}
+
+    ;
+
+    private static enum whoRun {COMP, GAMER}
+
+    ;
     private static Scanner scanner = new Scanner(System.in);
 
     public static class CoordinatyMaxLine {
@@ -51,9 +57,21 @@ public class MainApp {
         boolean notEndGame = true;
         System.out.println("Старт игры \"Крестики нолики\"");
         initGame();
-        //WAR_MAP[0][0] = COMP_CELL;
-//        WAR_MAP[1][0] = COMP_CELL;
-//        WAR_MAP[2][0] = COMP_CELL;
+
+//        WAR_MAP[2][3] = COMP_CELL;
+//        WAR_MAP[3][2] = COMP_CELL;
+//        WAR_MAP[4][1] = COMP_CELL;
+
+//        WAR_MAP[2][5] = COMP_CELL;
+//        //WAR_MAP[3][4] = COMP_CELL;
+//        WAR_MAP[4][3] = COMP_CELL;
+//
+//        WAR_MAP[5][7] = COMP_CELL;
+//
+//        WAR_MAP[1][1] = COMP_CELL;
+//        WAR_MAP[8][8] = COMP_CELL;
+//        WAR_MAP[9][9] = COMP_CELL;
+
 //        WAR_MAP[3][0] = COMP_CELL;
 //        WAR_MAP[4][0] = COMP_CELL;
 //
@@ -72,8 +90,12 @@ public class MainApp {
 //        WAR_MAP[3][3] = COMP_CELL;
 //        WAR_MAP[4][3] = COMP_CELL;
 
-//        printMap();
+//        printMap("COMP");
+//        checkRIGHT_DIAGONAL(COMP_CELL);
+//        checkHORIZONT(COMP_CELL);
 //        checkVERTICAL(COMP_CELL);
+//        checkLEFT_DIAGONAL(COMP_CELL);
+
         gameOver:
         do {
             runComputer();
@@ -102,14 +124,14 @@ public class MainApp {
     public static void printMap(String who) {
         System.out.println("WAR map Step:" + who);
         int[] header = new int[SIZE_CUBE];
-        for (int x = 0; x < SIZE_CUBE; x++) header[x] = x+1;
+        for (int x = 0; x < SIZE_CUBE; x++) header[x] = x + 1;
         System.out.println("0:" + Arrays.toString(header));
         for (int x = 0; x < SIZE_CUBE; x++) {
             System.out.println((x + 1) + ":" + Arrays.toString(WAR_MAP[x]));
         }
     }
 
-    public static void initGame(){
+    public static void initGame() {
         WAR_MAP = new String[SIZE_CUBE][SIZE_CUBE];
         for (int x = 0; x < SIZE_CUBE; x++) {
             for (int y = 0; y < SIZE_CUBE; y++) {
@@ -140,12 +162,12 @@ public class MainApp {
                 int x = Integer.parseInt(splitCoordinates[0]);
                 int y = Integer.parseInt(splitCoordinates[1]);
                 if ((x <= 0 || x > SIZE_CUBE) || (y <= 0 || y > SIZE_CUBE))
-                    System.out.print("Не корректные координаты, куб размером: " + SIZE_CUBE +  " -> ");
+                    System.out.print("Не корректные координаты, куб размером: " + SIZE_CUBE + " -> ");
                 else {
-                    if (WAR_MAP[y-1][x-1] != EMPTY_CELL)
+                    if (WAR_MAP[y - 1][x - 1] != EMPTY_CELL)
                         System.out.print("Указанная ячейка не пустая! Введите новые координаты: -> ");
                     else {
-                        WAR_MAP[y-1][x-1] = GAMER_CELL;
+                        WAR_MAP[y - 1][x - 1] = GAMER_CELL;
                         error = false;
                     }
                 }
@@ -158,12 +180,12 @@ public class MainApp {
     public static boolean chekGame(String variant, whoRun hod) {
         boolean LineWin = true;
         String checker;
-        if (hod==whoRun.COMP)
+        if (hod == whoRun.COMP)
             checker = COMP_CELL;
         else
             checker = GAMER_CELL;
 
-        switch (variant){
+        switch (variant) {
             case "HORIZONT":
                 LineWin = checkHORIZONT(checker);
                 break;
@@ -172,93 +194,124 @@ public class MainApp {
                 break;
 
             case "LEFT_DIAGONAL":
-                //System.out.println("LEFT_DIAGONAL");
+                LineWin = checkLEFT_DIAGONAL(checker);
                 break;
 
             case "RIGHT_DIAGONAL":
-                //System.out.println("RIGHT_DIAGONAL");
+                LineWin = checkRIGHT_DIAGONAL(checker);
                 break;
         }
-        //System.out.println(coordinatyMaxLine.toString());
         return LineWin;
     }
 
-    public static boolean checkVERTICAL(String checker){
-        CoordinatyMaxLine coordinatyMaxLine = new CoordinatyMaxLine(checker, "VERTICAL");
-        int[] pBegin = new int[]{-1,-1};
-        int[] pEnd = new int[]{-1,-1};
-        for (int x = 0; x < SIZE_CUBE; x++) {
-            int countLine=0;
-            for (int y = 0; y < SIZE_CUBE; y++) {
-                if (WAR_MAP[y][x] == checker) {
-                    if (pBegin[0] == -1) {
-                        pBegin[0]= x+1;
-                        pBegin[1] = y+1;
-                        pEnd[0] = x+1;
-                        pEnd[1] = y+1;
+    public static boolean checkRIGHT_DIAGONAL(String checker) {
+        int firstDiag = SIZE_CUBE - COUNT_WIN_LINE;
+        int countLine = 0;
+        int step = 0;
+        win:
+        do {
+            countLine = 0;
+            if (firstDiag >= 0) {
+                for (int x = firstDiag + COUNT_WIN_LINE - 1; x >= 0; x--) {
+                    int y = firstDiag + COUNT_WIN_LINE - 1 - x;
+                    if (WAR_MAP[y][x] == checker) {
+                        countLine++;
+                        //System.out.println("RIGHT_DIAGONAL >>> X:" + (x + 1) + "; Y:" + (y + 1) + "; VALUE:" + WAR_MAP[y][x] + "; COUNTER:" + countLine);
+                        if (countLine == COUNT_WIN_LINE) break win;
                     } else {
-                        pEnd[0] = x+1;
-                        pEnd[1] = y+1;
+                        countLine = 0;
+                        //System.out.println("X:" + (x + 1) + "; Y:" + (y + 1) + "; VALUE:" + WAR_MAP[x][y] + "; COUNTER:" + countLine);
                     }
-                    countLine++;
-                    if (countLine == COUNT_WIN_LINE) break;
-                } else {
-                    pBegin = new int[]{-1,-1};
-                    pEnd = new int[]{-1,-1};
                 }
+            } else {
+                for (int y = SIZE_CUBE + firstDiag + step; y-step > 0; y--) {
+                    int x = SIZE_CUBE - y + step;
+                    if (WAR_MAP[y][x] == checker) {
+                        countLine++;
+                        //System.out.println("RIGHT_DIAGONAL >>> X:" + (x + 1) + "; Y:" + (y + 1) + "; VALUE:" + WAR_MAP[y][x] + "; COUNTER:" + countLine);
+                        if (countLine == COUNT_WIN_LINE) break win;
+                    } else {
+                        countLine = 0;
+                        //System.out.println("X:" + (x + 1) + "; Y:" + (y + 1) + "; VALUE:" + WAR_MAP[y][x] + "; COUNTER:" + countLine);
+                    }
+                }
+                step++;
             }
-            if (countLine > coordinatyMaxLine.counterMax || countLine == COUNT_WIN_LINE) {
-                coordinatyMaxLine.counterMax = countLine;
-                coordinatyMaxLine.beginPoint = pBegin;
-                coordinatyMaxLine.endPoint = pEnd;
-            }
+            //System.out.println("RETURN:" + (countLine != COUNT_WIN_LINE) + "; COUNTER:" + countLine);
+            firstDiag--;
+        } while (firstDiag >= ((SIZE_CUBE - COUNT_WIN_LINE) * -1));
 
-            pBegin = new int[]{-1,-1};
-            pEnd = new int[]{-1,-1};
-
-            if (countLine == COUNT_WIN_LINE) break;
-        }
-        //System.out.println(coordinatyMaxLine.toString());
-        return (coordinatyMaxLine.counterMax != COUNT_WIN_LINE);
+        return (countLine != COUNT_WIN_LINE);
     }
 
-    public static boolean checkHORIZONT(String checker){
-        CoordinatyMaxLine coordinatyMaxLine = new CoordinatyMaxLine(checker, "HORIZONT");
-        int[] pBegin = new int[]{-1,-1};
-        int[] pEnd = new int[]{-1,-1};
-        for (int y = 0; y < SIZE_CUBE; y++) {
-            int countLine=0;
-            for (int x = 0; x < SIZE_CUBE; x++) {
-                if (WAR_MAP[y][x] == checker) {
-                    if (pBegin[0] == -1) {
-                        pBegin[0]= x+1;
-                        pBegin[1] = y+1;
-                        pEnd[0] = x+1;
-                        pEnd[1] = y+1;
+    public static boolean checkLEFT_DIAGONAL(String checker) {
+        int firstDiag = SIZE_CUBE - COUNT_WIN_LINE;
+        int countLine = 0;
+        win:
+        do {
+            countLine = 0;
+            if (firstDiag >= 0) {
+                for (int y = firstDiag; y < SIZE_CUBE; y++) {
+                    int x = y - firstDiag;
+                    if (WAR_MAP[y][x] == checker) {
+                        countLine++;
+                        //System.out.println("LEFT_DIAGONAL >>> X:" + (x + 1) + "; Y:" + (y + 1) + "; VALUE:" + WAR_MAP[y][x] + "; COUNTER:" + countLine);
+                        if (countLine == COUNT_WIN_LINE) break win;
                     } else {
-                        pEnd[0] = x+1;
-                        pEnd[1] = y+1;
+                        countLine = 0;
+                        //System.out.println("X:" + (x + 1) + "; Y:" + (y + 1) + "; VALUE:" + WAR_MAP[y][x] + "; COUNTER:" + countLine);
                     }
-                    countLine++;
-                    if (countLine == COUNT_WIN_LINE) break;
-                } else {
-                    pBegin = new int[]{-1,-1};
-                    pEnd = new int[]{-1,-1};
+                }
+            } else {
+                for (int x = (firstDiag * -1); x < SIZE_CUBE; x++) {
+                    int y = x - (firstDiag * -1);
+                    if (WAR_MAP[y][x] == checker) {
+                        countLine++;
+                        //System.out.println("LEFT_DIAGONAL >>> X:" + (x + 1) + "; Y:" + (y + 1) + "; VALUE:" + WAR_MAP[y][x] + "; COUNTER:" + countLine);
+                        if (countLine == COUNT_WIN_LINE) break win;
+                    } else {
+                        countLine = 0;
+                        //System.out.println("X:" + (x + 1) + "; Y:" + (y + 1) + "; VALUE:" + WAR_MAP[y][x] + "; COUNTER:" + countLine);
+                    }
                 }
             }
-            if (countLine > coordinatyMaxLine.counterMax || countLine == COUNT_WIN_LINE) {
-                coordinatyMaxLine.counterMax = countLine;
-                coordinatyMaxLine.beginPoint = pBegin;
-                coordinatyMaxLine.endPoint = pEnd;
+            //System.out.println("RETURN:" + (countLine != COUNT_WIN_LINE) + "; COUNTER:" + countLine);
+            firstDiag--;
+        } while (firstDiag >= ((SIZE_CUBE - COUNT_WIN_LINE) * -1));
+
+        return (countLine != COUNT_WIN_LINE);
+    }
+
+    public static boolean checkVERTICAL(String checker) {
+        int countLine = 0;
+        for (int x = 0; x < SIZE_CUBE; x++) {
+            countLine = 0;
+            for (int y = 0; y < SIZE_CUBE; y++) {
+                if (WAR_MAP[y][x] == checker) {
+                    countLine++;
+                    //System.out.println("VERTICAL >>> X:" + (x + 1) + "; Y:" + (y + 1) + "; VALUE:" + WAR_MAP[y][x] + "; COUNTER:" + countLine);
+                    if (countLine == COUNT_WIN_LINE) break;
+                } else countLine = 0;
             }
-
-            pBegin = new int[]{-1,-1};
-            pEnd = new int[]{-1,-1};
-
             if (countLine == COUNT_WIN_LINE) break;
         }
-        //System.out.println(coordinatyMaxLine.toString());
-        return (coordinatyMaxLine.counterMax != COUNT_WIN_LINE);
+        return (countLine != COUNT_WIN_LINE);
+    }
+
+    public static boolean checkHORIZONT(String checker) {
+        int countLine = 0;
+        for (int y = 0; y < SIZE_CUBE; y++) {
+            countLine = 0;
+            for (int x = 0; x < SIZE_CUBE; x++) {
+                if (WAR_MAP[y][x] == checker) {
+                    countLine++;
+                    //System.out.println("HORIZONT >>> X:" + (x + 1) + "; Y:" + (y + 1) + "; VALUE:" + WAR_MAP[y][x] + "; COUNTER:" + countLine);
+                    if (countLine == COUNT_WIN_LINE) break;
+                } else countLine = 0;
+            }
+            if (countLine == COUNT_WIN_LINE) break;
+        }
+        return (countLine != COUNT_WIN_LINE);
     }
 }
 
